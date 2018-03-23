cb93de28850249a8660393a559be07b1c553d3b8
KOUSINISHA <35996286+KOUSINISHA@users.noreply.github.com>
2018-03-23 16:38:58 +0530

    array.c

---
+++ array.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int=a,b;
+printf("Enter the number");
+scanf("%d%d,&a");
+printf("Enter the array of N element");
+scanf("%d%d,&b");
+{
+printf("print the element");
+}
+getch();
+};
