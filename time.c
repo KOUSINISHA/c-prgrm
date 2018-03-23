ce6eda6e6e35e23cd59a92be8f64a118c822d6e6
KOUSINISHA <35996286+KOUSINISHA@users.noreply.github.com>
2018-03-23 16:47:55 +0530

    time.c

---
+++ time.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+printf("print the minutes");
+scanf("%d%d,&a");
+printf("print the hours");
+scanf("%d%d,&b");
+{
+printf("print the time");
+return(0);
+}
+getch();
+}
